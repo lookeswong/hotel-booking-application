6a46eca862687e837e4d1cbd84375d808531ba97
j2-murnane <joseph2.murnane@live.uwe.ac.uk>
2020-02-06 09:26:22 +0000

    Lease Get/Setters

--- OOSD/src/oosd/Lease.java
+++ OOSD/src/oosd/Lease.java
@@ -5,10 +5,52 @@
  */
 package oosd;
 
+import java.util.Date;
+
 /**
  *
- * @author Lucas
+ * @author Joe Murnane
  */
 public class Lease {
+    private Date startDate;
+    private Student student;
+    private int leaseNo;
+    
+    public Lease() {
+        student = null;
+        leaseNo = 0;
+        startDate = null;
+    }
+    
+    public Lease(Date startDate, int leaseNo, Student student) {
+        this.startDate = startDate;
+        this.leaseNo = leaseNo;
+        this.student = student;
+        
+    }
+    
+    public Student getStudent(){
+        return this.student;
+    }
+    
+    // public Room getRoom() How to implement this? are we storing each room with lease and forgoty to add to uml ddiagram? or do I have to go back up to find the room this lease is associated with?
+    //
+    // }
+
+    public Date getStartDate(){
+        return this.startDate;
+    }
+    
+    public int getLeaseNo(){
+        return this.leaseNo;
+    }
+    
+    public void setStudent(Student student){
+        this.student = student;
+    }
+    
+    public void setStartDate(Date startDate){
+        this.startDate = startDate;
+    }
     
 }

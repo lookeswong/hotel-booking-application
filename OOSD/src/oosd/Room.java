0d28ea2efd31597e00e9938748e15085dc2ae745
j2-murnane <joseph2.murnane@live.uwe.ac.uk>
2020-02-06 09:26:41 +0000

    Room get/setters

--- OOSD/src/oosd/Room.java
+++ OOSD/src/oosd/Room.java
@@ -7,8 +7,64 @@ package oosd;
 
 /**
  *
- * @author Lucas
+ * @author Joe Murnane
  */
 public class Room {
+    private int roomNo;
+    private Lease lease;
+    private int cleanStatus;
+    private double rent;
+    private String description;
     
+    public Room(int roomNo, Lease lease, int cleanStatus, double rent, String description){
+        this.roomNo = roomNo;
+        this.lease = lease;
+        this.cleanStatus = cleanStatus;
+        this.rent = rent;
+        this.description = description;
+    }
+    
+    public int getRoomNo(){
+        return this.roomNo;
+    }
+    
+    public Lease getLease(){
+        return this.lease;
+    }
+    
+    public int getCleanStatus(){
+        return this.cleanStatus;
+    }
+    
+    public double getRent(){
+        return this.rent;
+    }
+    
+    public String getDescription(){
+        return this.description;
+    }
+    
+    public void setCleanStatus(int cleanStatus){
+        this.cleanStatus = cleanStatus;
+    }
+    
+    public void removeLease(){
+        this.lease = null; // better way to do this?
+    }
+    
+    public void addLease(Lease lease){
+        this.lease = lease;
+    }
+    
+    public void setRoomNo(int roomNo){
+        this.roomNo = roomNo;
+    }
+    
+    public void setRent(double rent){
+        this.rent = rent;
+    }
+    
+    public void setDescription(String description){
+        this.description = description;
+    }
 }
